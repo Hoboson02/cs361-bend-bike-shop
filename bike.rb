b1b8c2162f19c3ff661977869f796300d30ea622
Ryan Earl <ryanleoearl@gmail.com>
2021-11-14 20:02:31 -0800

    Split bike class into bike and cargo classes. Adjusted main.rb to account for the changes to the bike object.

--- bike.rb
+++ bike.rb
@@ -3,9 +3,8 @@
 class Bike
 
   STANDARD_WEIGHT = 200 # lbs
-  MAX_CARGO_ITEMS = 10
-
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
+  
+  attr_accessor :id, :color, :price, :weight, :rented
 
   def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
     @id = id
@@ -13,26 +12,37 @@ class Bike
     @price = price
     @weight = weight
     @rented = rented
-    @cargo_contents = []
   end
 
   def rent!
     self.rented = true
   end
 
-  def add_cargo(item)
+end
+
+class Cargo
+
+  MAX_CARGO_ITEMS = 10
+
+  attr_accessor :cargo_contents
+
+  def initialize()
+    @cargo_contents = []
+  end
+
+  def add(item)
     self.cargo_contents << item
   end
 
-  def remove_cargo(item)
+  def remove(item)
     self.cargo_contents.remove(item)
   end
 
-  def pannier_capacity
+  def capacity
     MAX_CARGO_ITEMS
   end
 
-  def pannier_remaining_capacity
+  def remaining_capacity
     MAX_CARGO_ITEMS - self.cargo_contents.size
   end
 
